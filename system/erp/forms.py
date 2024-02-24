d428cda525e44d469d722bd7c15db8b681df99a0
Alex <aleksandreremeev75@gmail.com>
2024-02-24 23:26:21 +0300

    feature:add href to customers page

--- system/erp/forms.py
+++ system/erp/forms.py
@@ -3,9 +3,14 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    ROLE_CHOICES = (
+        ('director', 'Директор'),
+        ('worker', 'Рабочий'),
+    )
+    role = forms.ChoiceField(choices=ROLE_CHOICES, required=True, help_text='Выберите вашу должность')
     class Meta:
         model = CustomUser
-        fields = ['email', 'first_name', 'last_name', 'password1', 'password2']
+        fields = ['email', 'first_name', 'last_name','role' ,'password1', 'password2']
 
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
